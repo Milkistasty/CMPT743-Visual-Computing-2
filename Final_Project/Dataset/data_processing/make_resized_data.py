07eb308669d10df0ea30df7878f027e21998fa74
Milkistasty <wenhewangcrane@gmail.com>
2024-02-15 13:45:20 -0800

    added the stylegan2_workshop-master

--- Final_Project/Dataset/data_processing/make_resized_data.py
+++
@@ -1,9 +0,0 @@
-import os
-from PIL import Image
-from tqdm import tqdm
-
-root_dir = "FFHQ/images1024x1024"
-
-for file in tqdm(os.listdir(root_dir)):
-    img = Image.open(root_dir+ "/"+file).resize((128, 128))
-    img.save("FFHQ_resized/"+file)
